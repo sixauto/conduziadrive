536ec0632b657d6e4080e5859b7c5e3c7a590837
Reginaldo Junior <reginaldo.junior@criticaltechworks.com>
2021-01-06 20:51:22 +0000

    remove .DS_Store

--- train.py
+++ train.py
@@ -5,12 +5,12 @@ from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common import make_vec_env
 from stable_baselines import PPO2
 
-env = make_vec_env('CarRacing-v0', n_envs=4)
+env = make_vec_env('CarRacing-v0', n_envs=2)
 drive = PPO2(MlpPolicy, env, verbose=1)
-drive.learn(total_timesteps=100)
+drive.learn(total_timesteps=125, log_interval=1, tb_log_name='PPO2', reset_num_timesteps=True)
 drive.save("conduziadrive")
 
-del drive
+#del drive
 
 drive = PPO2.load("conduziadrive")
 
