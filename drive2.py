06eb11e5ab78affeaaca5b7954040b55390de28c
Carlos Seco <carlosseco29@gmail.com>
2021-01-11 23:05:08 +0000

    save steps and rewards to file

--- drive2.py
+++ drive2.py
@@ -5,6 +5,18 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import CheckpointCallback, EveryNTimesteps
 
+import pickle
+
+# obj0, obj1, obj2 are created here...
+
+# Saving the objects:
+
+def load_vars():
+# Getting back the objects:
+    with open('objs.pkl') as f:  # Python 3: open(..., 'rb')
+        rewards, steps = pickle.load(f)
+        return rewards, steps
+
 def run():
     checkpoint_on_event = CheckpointCallback(save_freq=1, save_path='./logs/')
     event_callback = EveryNTimesteps(n_steps=500, callback=checkpoint_on_event)
@@ -22,11 +34,15 @@ def run():
     total_reward = 0.0
     steps = 0
     obs = env.reset()
+    rewards_steps = {}
     while True:
         action, _states = drive.predict(obs)
         obs, rewards, dones, info = env.step(action)
         total_reward += rewards
         steps += 1
+        rewards_steps[steps] = rewards
         env.render()
+    with open('objs.pkl', 'w') as f:  # Python 3: open(..., 'wb')
+        pickle.dump(rewards_steps, f)
 
 run()
