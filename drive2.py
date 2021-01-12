2908b40565694a11174a71f940f42e6a394628a7
Reginaldo Junior <reginaldo.junior@criticaltechworks.com>
2021-01-12 11:28:24 +0000

    add new coding

--- drive2.py
+++ drive2.py
@@ -1,5 +1,6 @@
 import gym
 gym.logger.set_level(32)
+import numpy as np
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
@@ -7,13 +8,8 @@ from stable_baselines3.common.callbacks import CheckpointCallback, EveryNTimeste
 
 import pickle
 
-# obj0, obj1, obj2 are created here...
-
-# Saving the objects:
-
 def load_vars():
-# Getting back the objects:
-    with open('objs.pkl') as f:  # Python 3: open(..., 'rb')
+    with open('objs.pkl', 'rb') as f:
         rewards, steps = pickle.load(f)
         return rewards, steps
 
@@ -31,18 +27,26 @@ def run():
 
     drive = PPO.load("conduziadrive")
 
-    total_reward = 0.0
+    score = 0.0
     steps = 0
     obs = env.reset()
+    running_score = 0
+    reward_threshold = 900
     rewards_steps = {}
     while True:
         action, _states = drive.predict(obs)
-        obs, rewards, dones, info = env.step(action)
-        total_reward += rewards
+        obs, reward, dones, info = env.step(action * np.array([2., 1., 1.]) + np.array([-1., 0., 0.]))
+        score += reward
         steps += 1
-        rewards_steps[steps] = rewards
-        env.render()
-    with open('objs.pkl', 'w') as f:  # Python 3: open(..., 'wb')
+        rewards_steps[steps] = reward
+        #env.render()
+        running_score = running_score * 0.99 + score * 0.01
+        print (running_score)
+        if running_score > reward_threshold:
+            print("Solved! Reward: {} and the last episode: {}!".format(running_score, score))
+            env.close()
+            break
+    with open('objs.pkl', 'wb') as f:
         pickle.dump(rewards_steps, f)
 
 run()
