0088d283e533afbc1b38e34a479997f198575e24
Reginaldo Junior <reginaldo.junior@criticaltechworks.com>
2021-01-12 22:08:11 +0000

    change the time steps

--- drive2.py
+++ drive2.py
@@ -1,10 +1,7 @@
 import gym
 gym.logger.set_level(32)
 import numpy as np
-
 from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import CheckpointCallback, EveryNTimesteps
-
 import pickle
 
 def load_vars():
@@ -13,12 +10,9 @@ def load_vars():
         return rewards, steps
 
 def run():
-    checkpoint_on_event = CheckpointCallback(save_freq=1, save_path='./logs/')
-    event_callback = EveryNTimesteps(n_steps=500, callback=checkpoint_on_event)
-
     env = gym.make('CarRacing-v0')
     drive = PPO('CnnPolicy', env, verbose=1)
-    drive.learn(int(400), callback=event_callback)
+    drive.learn(total_timesteps=100000)
     drive.save("conduziadrive")
 
     del drive
@@ -37,7 +31,7 @@ def run():
         score += reward
         steps += 1
         rewards_steps[steps] = reward
-        #env.render()
+        env.render()
         running_score = running_score * 0.99 + score * 0.01
         if running_score > reward_threshold:
             print("Solved! Reward: {} and the last episode: {}!".format(running_score, score))
