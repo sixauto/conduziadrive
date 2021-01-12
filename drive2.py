6cccd9756d96b9a156435e74a20f80796a156dd1
Reginaldo Junior <reginaldo.junior@criticaltechworks.com>
2021-01-12 17:48:40 +0000

    add for in load

--- drive2.py
+++ drive2.py
@@ -9,7 +9,7 @@ from stable_baselines3.common.callbacks import CheckpointCallback, EveryNTimeste
 import pickle
 
 def load_vars():
-    with open('objs.pkl', 'rb') as f:
+    with open('./objs.pkl', 'rb') as f:
         rewards, steps = pickle.load(f)
         return rewards, steps
 
@@ -20,33 +20,32 @@ def run():
     env = make_vec_env('CarRacing-v0', n_envs=1)
     drive = PPO('MlpPolicy', env, verbose=1)
 
-    drive.learn(int(2e4), callback=event_callback)
+    drive.learn(int(1000), callback=event_callback)
     drive.save("conduziadrive")
 
     del drive
 
     drive = PPO.load("conduziadrive")
 
-    score = 0
-    steps = 0
     obs = env.reset()
-    running_score = 0
-    reward_threshold = 900
-    rewards_steps = {}
     while True:
-        action, _states = drive.predict(obs)
-        obs, reward, dones, info = env.step(action)
-        score += reward
-        steps += 1
-        rewards_steps[steps] = reward
-        #env.render()
+        score = 0
+        steps = 0
+        running_score = 0
+        reward_threshold = 900
+        rewards_steps = {}
+        for t in range(1000):
+            action, _states = drive.predict(obs)
+            obs, reward, dones, info = env.step(action)
+            score += reward
+            steps += 1
+            rewards_steps[steps] = reward
+            env.render()
         running_score = running_score * 0.99 + score * 0.01
-        print (running_score)
         if running_score > reward_threshold:
             print("Solved! Reward: {} and the last episode: {}!".format(running_score, score))
             env.close()
             break
-    with open('objs.pkl', 'wb') as f:
+    with open('./objs.pkl', 'wb') as f:
         pickle.dump(rewards_steps, f)
-
 run()
