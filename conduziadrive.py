6bdbe7c6c1ccccb44eefc6a34e790b0e28a0a3ca
Reginaldo Junior <reginaldo.junior@criticaltechworks.com>
2021-01-16 15:32:26 +0000

    code to test in azure

--- conduziadrive.py
+++ conduziadrive.py
@@ -11,14 +11,13 @@ def train():
     #Cria o agent
     drive = PPO(MlpPolicy, env, gamma=0.9997, gae_lambda=1, ent_coef=0.01, vf_coef=1, batch_size=256, learning_rate=5e-6, clip_range=0.1, n_steps=32, n_epochs=20, verbose=1)
     # Treina o agent
-    total_timesteps = 40000
-    drive.learn(total_timesteps=total_timesteps)
+    drive = drive.learn(total_timesteps=10000)
     # Salva o treino feito pelo agent
-    drive.save("conduziadrive")
+    # drive.save("conduziadrive")
 
-    del drive
+    #del drive
     # Faz load automatico dos argumentos
-    drive = PPO.load("conduziadrive")
+    #drive = PPO.load("conduziadrive")
 
     # Execulta o agent
     score = 0
@@ -28,5 +27,6 @@ def train():
         obs, reward, done, info = env.step(action)
         score += reward
         env.render()
+    drive.save("conduziadrive")
 
 train()
