77a5c9afa3490cf8012cdd05557ddeeae2867363
Reginaldo Junior <reginaldo.junior@criticaltechworks.com>
2021-01-17 21:59:33 +0000

    add verbose

--- conduziadrive.py
+++ conduziadrive.py
@@ -10,7 +10,7 @@ def train():
     #Recebe e cria o ambiente
     env = make_vec_env('CarRacing-v0', seed=0, n_envs=6)
     #Cria e Treina o Agent
-    drive = PPO(MlpPolicy, env, gamma=0.9997, gae_lambda=1, ent_coef=0.01, vf_coef=1, batch_size=256, learning_rate=5e-6, clip_range=0.1, n_steps=4096, n_epochs=30).learn(total_timesteps=1600000)
+    drive = PPO(MlpPolicy, env, gamma=0.9997, gae_lambda=1, ent_coef=0.01, vf_coef=1, batch_size=256, learning_rate=5e-6, clip_range=0.1, n_steps=4096, n_epochs=30, verbose=1).learn(total_timesteps=1600000)
     # Salva o treino feito pelo Agent
     drive.save("conduziadrive")
 
