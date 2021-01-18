f07e5f6590e4cf52085f06c04aa60932d27859b4
Reginaldo Junior <reginaldo.junior@criticaltechworks.com>
2021-01-18 21:08:32 +0000

    add new values to trains

--- conduziadrive.py
+++ conduziadrive.py
@@ -8,21 +8,33 @@ from stable_baselines3.common.env_util import make_vec_env
 
 def train():
     #Recebe e cria o ambiente
-    env = make_vec_env('CarRacing-v0', n_envs=16)
+    env = gym.make('CarRacing-v0')
     #Cria o agent
-    drive = PPO(MlpPolicy, env, gamma=0.9997, gae_lambda=1, ent_coef=0.01, batch_size=128, learning_rate=5e-6, clip_range=0.1, n_epochs=20, verbose=1)
+    drive = PPO(MlpPolicy, env, gamma=0.9997, gae_lambda=1, ent_coef=0.01, vf_coef=1, batch_size=4096, learning_rate=5e-6, clip_range=0.1, n_steps=5000, n_epochs=100, target_kl=0.03, verbose=1)
     # Treina o agent
-    drive = drive.learn(total_timesteps=250000, eval_freq=200, n_eval_episodes=5, log_interval=1000).save("conduziadrive")
+    drive = drive.learn(total_timesteps=10000, log_interval=10).save("conduziadrive")
 
-    #del drive
+    del drive
     # Faz load automatico dos argumentos
-    #drive = PPO.load("conduziadrive")
-
+    drive = PPO.load("conduziadrive")
     # Executa o agent
-    #obs = env.reset()
-    #while True:
-    #    action, _states = drive.predict(obs)
-    #    obs, reward, done, info = env.step(action)
-    #    env.render()
+    rewards = []
+    obs = env.reset()
+    while True:
+        total_rewards = 0
+        obs = env.reset()
+        for t in range(1000):
+            action, _states = drive.predict(obs)
+            obs, reward, done, info = env.step(action)
+            total_rewards += reward
+            #env.render()
+            if(t % 100 == 0):
+                print(t)
+            if done or t == 999:
+                print("Episode {} finished after {} timesteps".format(5, t+1))
+                print("Reward: {}".format(total_rewards))
+                np.append(rewards, total_rewards)
+            if done:
+                break
 
 train()
