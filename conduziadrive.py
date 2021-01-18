0fc6c5c28eeb8e9c0d2c5c88c20f43418b02beff
Reginaldo Junior <reginaldo.junior@criticaltechworks.com>
2021-01-18 02:18:55 +0000

    add trainig with 16 envs

--- conduziadrive.py
+++ conduziadrive.py
@@ -8,31 +8,21 @@ from stable_baselines3.common.env_util import make_vec_env
 
 def train():
     #Recebe e cria o ambiente
-    env = make_vec_env('CarRacing-v0', seed=0, n_envs=6)
-    #Cria e Treina o Agent
-    drive = PPO(MlpPolicy, env, gamma=0.9997, gae_lambda=1, ent_coef=0.01, vf_coef=1, batch_size=256, learning_rate=5e-6, clip_range=0.1, n_steps=4096, n_epochs=30, verbose=1).learn(total_timesteps=1600000)
-    # Salva o treino feito pelo Agent
-    drive.save("conduziadrive")
+    env = make_vec_env('CarRacing-v0', n_envs=16)
+    #Cria o agent
+    drive = PPO(MlpPolicy, env, gamma=0.9997, gae_lambda=1, ent_coef=0.01,  vf_coef=0.5, batch_size=128, learning_rate=5e-6, clip_range=0.1, n_steps=128, n_epochs=20, verbose=1)
+    # Treina o agent
+    drive = drive.learn(total_timesteps=250000, eval_freq=200, n_eval_episodes=5, log_interval=1000).save("conduziadrive")
 
     #del drive
-    # Faz load automatico dos argumentos antes treinados
+    # Faz load automatico dos argumentos
     #drive = PPO.load("conduziadrive")
 
     # Executa o agent
-    #score = 0
-    #running_score = 0
-    #observation = env.reset()
+    #obs = env.reset()
     #while True:
+    #    action, _states = drive.predict(obs)
+    #    obs, reward, done, info = env.step(action)
     #    env.render()
-    #    action = drive.predict(observation)
-    #    observation, reward, done, info = env.step(action)
-    #    score += reward
-    #    if done.all():
-    #       score = 0
-    #       running_score = 0
-    #       observation = env.reset()
-    #running_score = running_score * 0.99 + score * 0.01
-    #print(running_score)
-    #env.close()
 
 train()
