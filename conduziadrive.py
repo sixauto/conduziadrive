f730f90fe996b12f1b3be04cbeadc729e63474e2
Ubuntu <azureuser@sixauto-drive.vmjmky2zjtiujah52rhhagysge.ax.internal.cloudapp.net>
2021-01-18 22:15:49 +0000

    add learn

--- conduziadrive.py
+++ conduziadrive.py
@@ -8,14 +8,13 @@ from stable_baselines3.common.env_util import make_vec_env
 
 def train():
     #Recebe e cria o ambiente
-    env = gym.make('CarRacing-v0')
-    env = make_vec_env (env, n_envs=6)
+    env = make_vec_env ('CarRacing-v0', n_envs=6)
     #Cria o agent
-    drive = PPO(MlpPolicy, env, gamma=0.9997, gae_lambda=1, ent_coef=0.01, vf_coef=1, batch_size=4096, learning_rate=5e-6, clip_range=0.1, n_steps=5000, n_epochs=100, target_kl=0.03, verbose=1)
+    #drive = PPO(MlpPolicy, env, gamma=0.9997, gae_lambda=1, ent_coef=0.01, vf_coef=1, batch_size=4096, learning_rate=5e-6, clip_range=0.1, n_steps=5000, n_epochs=100, target_kl=0.03, verbose=1)
     # Treina o agent
-    drive = drive.learn(total_timesteps=250000, log_interval=10).save("conduziadrive")
+    #drive = drive.learn(total_timesteps=250000, log_interval=10).save("conduziadrive")
 
-    del drive
+    #del drive
     # Faz load automatico dos argumentos
     drive = PPO.load("conduziadrive")
     # Executa o agent
@@ -31,11 +30,11 @@ def train():
             #env.render()
             if(t % 100 == 0):
                 print(t)
-            if done or t == 999:
+            if done.any or t == 999:
                 print("Episode {} finished after {} timesteps".format(5, t+1))
                 print("Reward: {}".format(total_rewards))
                 np.append(rewards, total_rewards)
-            if done:
+            if done.any:
                 break
 
 train()
