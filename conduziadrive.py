7341952213bc6f1c968cfdf9a548766befea1ef6
Reginaldo Junior <reginaldo.junior@criticaltechworks.com>
2021-01-18 02:25:21 +0000

    add log interval

--- conduziadrive.py
+++ conduziadrive.py
@@ -10,7 +10,7 @@ def train():
     #Recebe e cria o ambiente
     env = make_vec_env('CarRacing-v0', n_envs=16)
     #Cria o agent
-    drive = PPO(MlpPolicy, env, gamma=0.9997, gae_lambda=1, ent_coef=0.01,  vf_coef=0.5, batch_size=128, learning_rate=5e-6, clip_range=0.1, n_steps=128, n_epochs=20, verbose=1)
+    drive = PPO(MlpPolicy, env, gamma=0.9997, gae_lambda=1, ent_coef=0.01, batch_size=128, learning_rate=5e-6, clip_range=0.1, n_epochs=20, verbose=1)
     # Treina o agent
     drive = drive.learn(total_timesteps=250000, eval_freq=200, n_eval_episodes=5, log_interval=1000).save("conduziadrive")
 
