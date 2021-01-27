26c6172a2037f90c19cd6c93badba016ea818388
Reginaldo Junior <reginaldo.junior@criticaltechworks.com>
2021-01-27 18:31:28 +0000

    add changes to conduzia drive

--- conduziadrive.py
+++ conduziadrive.py
@@ -1,13 +1,22 @@
 import gym
-gym.logger.set_level(32)
+gym.logger.set_level(40)
 
 import pybullet
+import numpy as np
 from typing import Callable
+import matplotlib.pyplot as plt
+from pyvirtualdisplay import Display
 
 from stable_baselines3 import PPO
 from stable_baselines3.ppo import MlpPolicy
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+from stable_baselines3.common.callbacks import CheckpointCallback
+
+def virtual_display():
+    display = Display(visible=False, size=(100, 60), color_depth=24)
+    display.start()
 
 def linear_schedule(initial_value: float) -> Callable[[float], float]:
     def func(progress_remaining: float) -> float:
@@ -15,43 +24,66 @@ def linear_schedule(initial_value: float) -> Callable[[float], float]:
     return func
 
 def train():
-    # Get the enviroment from GYM
     env = DummyVecEnv([lambda: gym.make("CarRacing-v0")])
-    # Automatically normalize the input features and reward
     env = VecNormalize(env, norm_obs=True, norm_reward=True, gamma=0.9997, clip_obs=10., epsilon=0.1)
-    # Create the Agent for training
-    drive = PPO(MlpPolicy, env, gamma=0.9997, ent_coef=0.01, vf_coef=1, batch_size=128, learning_rate=linear_schedule(0.001), clip_range=linear_schedule(0.1), n_steps=1000, n_epochs=20, verbose=1)
-    # Training the Agent
-    drive.learn(total_timesteps=50000)
-    # Save the Training
-    drive.save("conduziadrive")
-    env.save("conduziadrive.pkl")
+
+    drive = PPO(MlpPolicy, env, ent_coef=0.01, vf_coef=1, batch_size=128, learning_rate=linear_schedule(0.001), clip_range=linear_schedule(0.1), n_steps=1000, n_epochs=20, verbose=1)
+
+    checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='./logs/', name_prefix='drive_checkpoint')
+
+    drive.learn(total_timesteps=40000, callback=checkpoint_callback)
     env.close()
+    drive.learn(total_timesteps=40000, callback=checkpoint_callback, reset_num_timesteps=False)
+    env.close()
+    drive.learn(total_timesteps=40000, callback=checkpoint_callback, reset_num_timesteps=False)
+    env.close()
+    drive.learn(total_timesteps=40000, callback=checkpoint_callback, reset_num_timesteps=False)
+    env.close()
+    drive.learn(total_timesteps=40000, callback=checkpoint_callback, reset_num_timesteps=False)
+    env.close()
+    drive.learn(total_timesteps=40000, callback=checkpoint_callback, reset_num_timesteps=False)
+    env.close()
+    drive.learn(total_timesteps=40000, callback=checkpoint_callback, reset_num_timesteps=False)
+    env.close()
+    drive.learn(total_timesteps=40000, callback=checkpoint_callback, reset_num_timesteps=False)
+
+    drive.save("drive_train")
+
+    mean_reward, std_reward = evaluate_policy(drive, env, n_eval_episodes=10)
+    print(f'Mean reward: {mean_reward} +/- {std_reward:.2f}')
 
 def run():
-    # Load the traininig
-    drive = PPO.load("conduziadrive")
-    # Get the enviroment and normalize this enviroment for execution
+    drive = PPO.load("./logs/drive_checkpoint_320000_steps")
+
     env = DummyVecEnv([lambda: gym.make("CarRacing-v0")])
-    env = VecNormalize.load("conduziadrive.pkl", env)
-    #  do not update them at test time
-    env.training = False
-    # Reset the enviroment
+    env = VecNormalize(env, norm_obs=True, norm_reward=True, gamma=0.9997, clip_obs=10., epsilon=0.1)
+
+    mean_reward, std_reward = evaluate_policy(drive, env, n_eval_episodes=10)
+    print(f'Mean reward: {mean_reward} +/- {std_reward:.2f}')
+
+    rewards = []
+    total_reward = 0
+
     obs = env.reset()
     while True:
         obs = env.reset()
-        total_rewards = 0
+
+        mean_reward, std_reward = evaluate_policy(drive, env, n_eval_episodes=10)
+        print(f'Mean reward: {mean_reward} +/- {std_reward:.2f}')
+
         for t in range(1000):
-            # Receive the action and states of the agent trained before
             action, _states = drive.predict(obs, deterministic=True)
             obs, reward, done, info = env.step(action)
-            total_rewards += reward
-            # Rendering the enviroment to show this in graph scale
             env.render()
-            # Validate if the agent get the "goal" or "die" after a failed round
-            if done:
-                print("Episode {} finished after {} timesteps".format(5, t+1))
-                print("Reward: {}".format(total_rewards))
-    env.close()
+            total_reward += reward
+        if(t % 100 == 0):
+            print(t)
+        if done or t == 999:
+            print("Finished after {} timesteps".format(t+1))
+            print("Reward: {}".format(total_reward))
+            rewards.append(total_reward)
+            env.close()
 
-train()
+virtual_display()
+#train()
+run()
